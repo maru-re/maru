0a7893f8c14fa6c86bdd5fdfe3ea0b3dc33df386
KazariEX <1364035137@qq.com>
2024-10-02 19:08:05 +0800

    fix: ん before vowel should be `n'`

--- packages/romaji/src/utils/transformOshimai.ts
+++ packages/romaji/src/utils/transformOshimai.ts
@@ -3,9 +3,13 @@ import type { RomajiToken } from '../types'
 export function transformOshimai(tokens: RomajiToken[]) {
   for (let i = 0; i < tokens.length; i++) {
     const token = tokens[i]!
+    const next = tokens[i + 1]
     if (token.source !== 'ん')
       continue
 
+    if (next && /^[aeiouy]/.test(next.value)) {
+      token.value += '\''
+    }
     token.mergeForwards = true
   }
 }
--- packages/romaji/test/transform.test.ts
+++ packages/romaji/test/transform.test.ts
@@ -8,43 +8,41 @@ describe('transformOnbiki', () => {
   it('onbiki', () => {
     const tokens = createTokens('ねー')
     transformOnbiki(tokens)
-    expect(tokens)
-      .toMatchInlineSnapshot(`
-        [
-          {
-            "index": 0,
-            "source": "ね",
-            "value": "ne",
-          },
-          {
-            "index": 1,
-            "mergeForwards": true,
-            "source": "ー",
-            "value": "e",
-          },
-        ]
-      `)
+    expect(tokens).toMatchInlineSnapshot(`
+      [
+        {
+          "index": 0,
+          "source": "ね",
+          "value": "ne",
+        },
+        {
+          "index": 1,
+          "mergeForwards": true,
+          "source": "ー",
+          "value": "e",
+        },
+      ]
+    `)
   })
 
   it('kogakimoji', () => {
     const tokens = createTokens('ねぇ')
     transformOnbiki(tokens)
-    expect(tokens)
-      .toMatchInlineSnapshot(`
-        [
-          {
-            "index": 0,
-            "source": "ね",
-            "value": "ne",
-          },
-          {
-            "index": 1,
-            "mergeForwards": true,
-            "source": "ぇ",
-            "value": "e",
-          },
-        ]
-      `)
+    expect(tokens).toMatchInlineSnapshot(`
+      [
+        {
+          "index": 0,
+          "source": "ね",
+          "value": "ne",
+        },
+        {
+          "index": 1,
+          "mergeForwards": true,
+          "source": "ぇ",
+          "value": "e",
+        },
+      ]
+    `)
   })
 })
 
@@ -52,22 +50,41 @@ describe('transformOshimai', () => {
   it('basic', () => {
     const tokens = createTokens('ねん')
     transformOshimai(tokens)
-    expect(tokens)
-      .toMatchInlineSnapshot(`
-        [
-          {
-            "index": 0,
-            "source": "ね",
-            "value": "ne",
-          },
-          {
-            "index": 1,
-            "mergeForwards": true,
-            "source": "ん",
-            "value": "n",
-          },
-        ]
-      `)
+    expect(tokens).toMatchInlineSnapshot(`
+      [
+        {
+          "index": 0,
+          "source": "ね",
+          "value": "ne",
+        },
+        {
+          "index": 1,
+          "mergeForwards": true,
+          "source": "ん",
+          "value": "n",
+        },
+      ]
+    `)
+  })
+
+  it('before vowel', () => {
+    const tokens = createTokens('んあ')
+    transformOshimai(tokens)
+    expect(tokens).toMatchInlineSnapshot(`
+      [
+        {
+          "index": 0,
+          "mergeForwards": true,
+          "source": "ん",
+          "value": "n'",
+        },
+        {
+          "index": 1,
+          "source": "あ",
+          "value": "a",
+        },
+      ]
+    `)
   })
 })
 
@@ -75,21 +92,20 @@ describe('transformSokuon', () => {
   it('basic', () => {
     const tokens = createTokens('って')
     transformSokuon(tokens)
-    expect(tokens)
-      .toMatchInlineSnapshot(`
-        [
-          {
-            "index": 0,
-            "source": "っ",
-            "value": "t",
-          },
-          {
-            "index": 1,
-            "mergeForwards": true,
-            "source": "て",
-            "value": "te",
-          },
-        ]
-      `)
+    expect(tokens).toMatchInlineSnapshot(`
+      [
+        {
+          "index": 0,
+          "source": "っ",
+          "value": "t",
+        },
+        {
+          "index": 1,
+          "mergeForwards": true,
+          "source": "て",
+          "value": "te",
+        },
+      ]
+    `)
   })
 })
