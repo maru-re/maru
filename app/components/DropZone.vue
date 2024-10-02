4095db8a640ac74441c9cddfa99ba306df2402d3
SerKo <44749100+serkodev@users.noreply.github.com>
2024-10-02 22:42:23 +0800

    refactor: improve drag and drop for `DropZone` (#9)

--- app/components/DropZone.vue
+++ app/components/DropZone.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 const isDragging = ref(false)
 
+// Use for track drag state
+let dragCounter = 0
+
+function isFileDrag(e: DragEvent) {
+  return e.dataTransfer?.types && e.dataTransfer.types.includes('Files')
+}
+
 async function onDrop(e: DragEvent) {
   e.preventDefault()
+  if (!isFileDrag(e))
+    return
+
   isDragging.value = false
+  dragCounter = 0
 
   await importFromFiles(
     [...e.dataTransfer?.items || []].map(i => i.webkitGetAsEntry()).filter(x => !!x)
@@ -11,21 +22,35 @@ async function onDrop(e: DragEvent) {
   )
 }
 
-function onDragOver(e: DragEvent) {
+function onDragEnter(e: DragEvent) {
   e.preventDefault()
+  if (!isFileDrag(e))
+    return
+
+  dragCounter++
   isDragging.value = true
 }
 
-function onDragExit() {
-  isDragging.value = false
+function onDragLeave(e: DragEvent) {
+  e.preventDefault()
+  if (!isFileDrag(e))
+    return
+
+  dragCounter--
+  if (dragCounter === 0) {
+    isDragging.value = false
+  }
+}
+
+function onDragOver(e: DragEvent) {
+  e.preventDefault()
 }
 
 onMounted(() => {
-  useEventListener('drop', onDrop)
+  useEventListener('dragenter', onDragEnter)
   useEventListener('dragover', onDragOver)
-  useEventListener('dragend', onDragExit)
-  useEventListener('pointerleave', onDragExit)
-  // useEventListener('dragleave', onDragExit)
+  useEventListener('dragleave', onDragLeave)
+  useEventListener('drop', onDrop)
 })
 </script>
 
