858718a377b6a952a13b3d8dde894f52b8d3147f
SerKo <serko.dev@gmail.com>
2024-10-10 03:51:28 +0800

    fix: floating items z-index

--- app/app.vue
+++ app/app.vue
@@ -15,7 +15,7 @@ useHead({
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <div fixed right-3 top-3 flex="~ gap-2" lt-lg="hidden">
+  <div fixed right-3 top-3 flex="~ gap-2" z-floating lt-lg="hidden">
     <DarkToggle class="p2 floating-glass" />
     <I18nSelector class="p2 floating-glass" />
   </div>
