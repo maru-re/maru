6f4ee6d2022b502c4dedf4210f454807b965960a
SerKo <serko.dev@gmail.com>
2024-10-09 23:26:19 +0800

    feat: add i18n selector

---
+++ app/components/atomic/I18nSelector.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Menu } from 'floating-vue'
+
+const { locale, locales, setLocale } = useI18n()
+</script>
+
+<template>
+  <Menu placement="top">
+    <IconButton icon="i-ph-translate" />
+    <template #popper>
+      <div p2 flex="~ col gap-1">
+        <SimpleButton
+          v-for="_locale in locales" :key="_locale.code"
+          :class="_locale.code !== locale && 'op50'"
+          :title="_locale.name"
+          @click="setLocale(_locale.code)"
+        />
+      </div>
+    </template>
+  </Menu>
+</template>
