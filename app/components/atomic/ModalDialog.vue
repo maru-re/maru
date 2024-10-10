88895effcf23503dacfa3742d15cc21c12fb598a
SerKo <serko.dev@gmail.com>
2024-10-10 18:37:01 +0800

    fix: click empty space of modal dialog

--- app/components/atomic/ModalDialog.vue
+++ app/components/atomic/ModalDialog.vue
@@ -18,9 +18,8 @@ function onClickEmptySpace() {
   <div
     v-show="value"
     class="fixed inset-0 z-dialog bg-black bg-opacity-50 backdrop-blur-5"
-    @click="onClickEmptySpace()"
   >
-    <div fixed inset-0 flex items-center justify-center p10>
+    <div fixed inset-0 flex items-center justify-center p10 @click.self="onClickEmptySpace()">
       <div
         class="max-h-80vh w-full of-auto rounded-lg p-6 floating-glass bg-base"
         :class="dialogClass"
