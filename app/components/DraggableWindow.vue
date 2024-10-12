e2a20fda5699000db1a4adaec91e6e8395e8785d
Oliver Mak <oliver139.working@gmail.com>
2024-10-12 16:52:29 +0800

    Avoid the draggable window stayed outside when resizing the browser window

--- app/components/DraggableWindow.vue
+++ app/components/DraggableWindow.vue
@@ -2,31 +2,61 @@
 interface Props {
   x?: number
   y?: number
+  maxInset?: number
 }
 const props = withDefaults(defineProps<Props>(), {
   x: 0,
   y: 0,
+  maxInset: 0,
 })
-const window = ref<HTMLElement | null>(null)
-const handle = ref<HTMLElement | null>(null)
 
-const { style } = useDraggable(window, {
+// #region : Setup draggable
+const dragWindow = ref<HTMLElement | null>(null)
+const dragHandle = ref<HTMLElement | null>(null)
+
+const { x: dragX, y: dragY, style } = useDraggable(dragWindow, {
   initialValue: { x: props.x, y: props.y },
-  handle,
+  handle: dragHandle,
   preventDefault: true,
 })
+// #endregion
+
+// #region : Avoid the window stayed outside
+const { width: windowWidth, height: windowHeight } = useWindowSize()
+const dragWindowBounding = useElementBounding(dragWindow)
+
+function limitPosition() {
+  if (dragWindowBounding.left.value < props.maxInset) {
+    dragX.value = props.maxInset
+  }
+  else if (dragWindowBounding.right.value > (windowWidth.value - props.maxInset)) {
+    dragX.value = windowWidth.value - props.maxInset - dragWindowBounding.width.value
+  }
+
+  if (dragWindowBounding.top.value < props.maxInset) {
+    dragY.value = props.maxInset
+  }
+  else if (dragWindowBounding.bottom.value > (windowHeight.value - props.maxInset)) {
+    dragY.value = windowHeight.value - props.maxInset - dragWindowBounding.height.value
+  }
+}
+
+onMounted(() => {
+  useEventListener('resize', () => limitPosition())
+})
+// #endregion
 </script>
 
 <template>
   <div
-    ref="window"
+    ref="dragWindow"
     class="draggable-window"
     fixed
     :style
     z-floating
   >
     <div
-      ref="handle"
+      ref="dragHandle"
       hover="op100 bg-gray/10"
       flex="~ items-center justify-center"
       draggable mxa w-20 cursor-move rounded-full op25
