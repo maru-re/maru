9edc00d863811bf6a227688775aa5a07bc92245d
Oliver Mak <oliver139.working@gmail.com>
2024-10-12 12:35:35 +0800

    feat: enable dragging the video in editor (#38)
    Co-authored-by: Anthony Fu <github@antfu.me>

---
+++ app/components/DraggableWindow.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface Props {
+  x?: number
+  y?: number
+}
+const props = withDefaults(defineProps<Props>(), {
+  x: 0,
+  y: 0,
+})
+const window = ref<HTMLElement | null>(null)
+const handle = ref<HTMLElement | null>(null)
+
+const { style } = useDraggable(window, {
+  initialValue: { x: props.x, y: props.y },
+  handle,
+  preventDefault: true,
+})
+</script>
+
+<template>
+  <div
+    ref="window"
+    class="draggable-window"
+    fixed
+    :style
+    z-floating
+  >
+    <div
+      ref="handle"
+      hover="op100 bg-gray/10"
+      flex="~ items-center justify-center"
+      draggable mxa w-20 cursor-move rounded-full op25
+    >
+      <div i-mdi-drag-horizontal ma text-size-xl />
+    </div>
+    <slot />
+  </div>
+</template>
