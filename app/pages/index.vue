7b0af12935a0cdefb8ab3dba3037c429bbebd3e7
Anthony Fu <github@antfu.me>
2024-10-15 02:06:37 +0800

    fix: z-index order

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -138,7 +138,7 @@ onMounted(() => {
       <div i-uil-flask flex-none text-base />
       {{ $t("messages.wip") }}
     </div>
-    <div v-if="collections.length > 0" sticky left-0 right-0 top-0 z-floating flex py3>
+    <div v-if="collections.length > 0" sticky left-0 right-0 top-0 z-search flex py3>
       <div
         flex="~ gap-3 items-center"
         z-search ma h-3em max-w-100 w-full of-hidden px3 floating-glass
