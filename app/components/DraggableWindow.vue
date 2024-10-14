7fbeaff7f5d6e597b5d336f25872021ef60c6fac
Oliver Mak <oliver139.working@gmail.com>
2024-10-14 13:07:45 +0800

    fix: Avoid draggable window staying outside #41 (#42)
    Co-authored-by: 山吹色御守 <85992002+KazariEX@users.noreply.github.com>

--- app/components/DraggableWindow.vue
+++ app/components/DraggableWindow.vue
@@ -2,24 +2,55 @@
 interface Props {
   x?: number
   y?: number
+  maxInset?: number
 }
 const props = withDefaults(defineProps<Props>(), {
   x: 0,
   y: 0,
+  maxInset: 0,
 })
-const window = ref<HTMLElement | null>(null)
-const handle = ref<HTMLElement | null>(null)
 
-const { style, isDragging } = useDraggable(window, {
+const dragWindow = ref<HTMLElement | null>(null)
+const dragHandle = ref<HTMLElement | null>(null)
+
+const { x: dragX, y: dragY, style, isDragging } = useDraggable(dragWindow, {
   initialValue: { x: props.x, y: props.y },
-  handle,
+  handle: dragHandle,
   preventDefault: true,
+  onEnd: limitPosition,
 })
+// #endregion
+
+// #region : Avoid the window stayed outside
+const { width: windowWidth, height: windowHeight } = useWindowSize()
+const dragWindowBounding = useElementBounding(dragWindow)
+
+function limitPosition() {
+  if (dragWindowBounding.left.value < props.maxInset) {
+    dragX.value = props.maxInset
+  }
+  else if (dragWindowBounding.right.value > (windowWidth.value - props.maxInset)) {
+    dragX.value = windowWidth.value - props.maxInset - dragWindowBounding.width.value
+  }
+
+  if (dragWindowBounding.top.value < props.maxInset) {
+    dragY.value = props.maxInset
+  }
+  else if (dragWindowBounding.bottom.value > (windowHeight.value - props.maxInset)) {
+    dragY.value = windowHeight.value - props.maxInset - dragWindowBounding.height.value
+  }
+}
+
+onMounted(() => {
+  const limitPositionDebounced = useDebounceFn(limitPosition, 250)
+  useEventListener('resize', limitPositionDebounced, { passive: true })
+})
+// #endregion
 </script>
 
 <template>
   <div
-    ref="window"
+    ref="dragWindow"
     class="draggable-window"
     :class="{
       'pointer-events-none': isDragging,
@@ -29,7 +60,7 @@ const { style, isDragging } = useDraggable(window, {
     z-floating
   >
     <div
-      ref="handle"
+      ref="dragHandle"
       hover="op100 bg-gray/10"
       flex="~ items-center justify-center"
       draggable pointer-events-auto mxa w-20 cursor-move rounded-full op25
